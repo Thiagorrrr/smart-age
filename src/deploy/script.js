b86615eac951c425fcbdf52a497169bc767ff382
Thiagorrrr <thiago.pereira.053@gmail.com>
2021-01-05 14:16:32 -0300

    adicionado conversor de tag img para php

---
+++ src/deploy/script.js
@@ -0,0 +1,39 @@
+let count = 0;
+
+function getHtml() {
+    let htmlComplete = document.getElementById('description1');
+    let htmlNew = document.getElementById('description2');
+    let htmlFinal = document.getElementById('description3');
+
+    const domparser = new DOMParser()
+    var parsed = domparser.parseFromString(htmlComplete.value.toString(), 'text/html');
+
+    // conteudo html
+    let rootParsed = parsed.getRootNode();
+    htmlNew.appendChild(rootParsed.body);
+
+
+    if (count === 0) {
+        component();
+        htmlFinal.value = htmlNew.innerHTML;
+        count++;
+    }
+}
+
+function component() {
+    let src = document.getElementsByTagName('img');
+    let source = document.getElementsByTagName('source');
+    let format = (data, type) => {
+
+        for (let index = 0; index < data.length; index++) {
+            if (type === 'img') {
+                data[index].src  = "<?php echo $block->getViewFileUrl( images" + data[index].src  + " '); ?>"
+            }else {
+                data[index].srcset  = "<?php echo $block->getViewFileUrl( images" + data[index].srcset  + " '); ?>"
+            }
+        }
+    }
+
+    format(src, 'img');
+    format(source, 'source');
+}
